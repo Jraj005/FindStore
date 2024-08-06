cbd0b63036435f14ffa39b58ca816c4d123257fa
Jayrajsinh Chauhan <155800774+Jraj005@users.noreply.github.com>
2024-08-06 20:05:18 +0530

    Update Login.jsx

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -19,7 +19,7 @@ function Login() {
     };
     // to call api to store data
     await axios
-      .post("http://localhost:4001/user/login", userInfo)
+      .post("https://findstore-backend.onrender.com/user/login", userInfo)
       .then((res) => {
         console.log(res.data);
         if (res.data) {
